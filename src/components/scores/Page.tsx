a60c066bc4f4ef9c74a94bfacc705a593b4c7e96
mogmyij <foonglucas@gmail.com>
2023-10-08 00:22:15 +0800

    changed title and icon of webPage

--- src/components/scores/Page.tsx
+++ src/components/scores/Page.tsx
@@ -1,34 +1,23 @@
 import { score, Columns } from "./Columns";
 import { DataTable } from "./DataTable";
-import databaseCadetScores from "../../services/databaseCadetScores"
+import databaseCadetScores from "../../services/databaseCadetScores";
 
 async function getData(): Promise<score[]> {
 	// Fetch data from your API here.
-  const userScore = await databaseCadetScores.getCadetScores()
-  userScore.map(
-    curr=>{
-      console.log(curr);
-    }
-  )
+	const userScore = await databaseCadetScores.getCadetScores();
+	// create score array that will be returned
+	var userDataTable:score[]=[];
+	// map each userScore object into a score object and add it to the array
+	userScore.map(curr=>{
+		const databaseScores:score = {
+			name: curr.user.name,
+			nric: curr.user.nric,
+			totalScore: curr.totalScore,
+		}
+		userDataTable.push(databaseScores)
+	})
 
-	return [
-		{
-			name: "test",
-			nric: "test",
-			totalScore: 20,
-		},
-		{
-			name: "test",
-			nric: "test",
-			totalScore: 20,
-		},
-		{
-			name: "test",
-			nric: "test",
-			totalScore: 20,
-		},
-		// ...
-	];
+	return userDataTable
 }
 
 export default async function DemoPage() {
--- src/services/databaseCadetScores.ts
+++ src/services/databaseCadetScores.ts
@@ -3,7 +3,20 @@ import { response } from "express";
 
 const baseURL = "http://localhost:3001/api";
 
-async function getCadetScores(): Promise<object[]> {
+//types for when data is recieved from the server
+type user = {
+	name: string,
+	nric: string,
+}
+//the database will link the user object and userScore object relationally and send
+//it to the web app. since we dont need all the key value pairs we declare this type
+//to only take note of the important keys we are interested in
+type userScoreData ={
+	totalScore: number,
+	user: user,
+}
+
+async function getCadetScores(): Promise<userScoreData[]> {
 	try {
 		const response = await axios.get(baseURL + "/userScore");
 		return response.data;
